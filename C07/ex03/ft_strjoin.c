a6a224b7e02a7c4c45e25a93fd7397a0782ba2be
Yahya Amaiche <yaamaich@c2r5p9.1337.ma>
2024-07-18 04:27:05 +0100

    mister

---
+++ C07/ex03/ft_strjoin.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaamaich <yaamaich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 02:29:48 by yaamaich          #+#    #+#             */
+/*   Updated: 2024/07/18 03:19:25 by yaamaich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*s;
+	int		i;
+	int		j;
+	int		c;
+
+	s = malloc(sizeof(strs));
+	i = 0;
+	c = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j] != '\0')
+		{
+			s[c++] = strs[i][j++];
+		}
+		j = 0;
+		while (sep[j] != '\0' && i != size - 1)
+		{
+			s[c++] = sep[j++];
+		}
+		i++;
+	}
+	s[c] = '\0';
+	return (s);
+}
+
+/*
+int	main(void)
+{
+	char	*tab[4];
+	tab[0] = "yahya";
+	tab[1] = "mohamed";
+	tab[2] = "lll";
+	tab[3] = "my life";
+	printf("%s", ft_strjoin(4, tab, " "));
+	return (0);
+}*/
