7f937e1351af27fa5ef1fed473529664930ec314
Yahya Amaiche <yaamaich@c1r7p4.1337.ma>
2025-02-17 22:52:21 +0100

    push swap

---
+++ 42-cursus/circle-three/push-swap/push-swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+typedef struct stack1
+{
+	int *next;
+	int *data;
+}			t_list;
+int main(int argc , char **argv)
+{
+	if (argc >= 2)
+	{
+		int i = 0;
+		while (argc && **argv)
+		{
+			t_list *stack1;
+		}
+	}
+}
