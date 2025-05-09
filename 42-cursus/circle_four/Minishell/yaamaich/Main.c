ad860a4bbf8c0670127e64f3e586ff30edcd0890
Yahya Amaiche <yaamaich@c2r1p2.1337.ma>
2025-05-09 22:26:50 +0100

    push to home

--- 42-cursus/circle_four/Minishell/yaamaich/Main.c
+++ 42-cursus/circle_four/Minishell/yaamaich/Main.c
@@ -6,7 +6,7 @@
 /*   By: yaamaich <yaamaich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:23:49 by yaamaich          #+#    #+#             */
-/*   Updated: 2025/04/30 23:12:13 by yaamaich         ###   ########.fr       */
+/*   Updated: 2025/05/09 22:15:45 by yaamaich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,61 +14,61 @@
 
 int whitespaces(char str)
 {
-    if (str[i] == 32 || str[i] >= 9 && str[i] <= 13)
-        return (1);
-    return (0);
+	if (str[i] == 32 || str[i] >= 9 && str[i] <= 13)
+		return (1);
+	return (0);
 }
 t_lexer *initialize_lexer(char *string)
 {
-    t_lexer *lexer;
-    
-    lexer = malloc(sizeof(t_lexer));
-    lexer->input = string;
-    lexer->position = 0;
-    lexer->length = ft_strlen(string);
-    return lexer;
+	t_lexer *lexer;
+	
+	lexer = malloc(sizeof(t_lexer));
+	lexer->input = string;
+	lexer->position = 0;
+	lexer->length = ft_strlen(string);
+	return lexer;
 }
 char    *get_next_token(t_lexer *lexer)
 {
-    char current_char; 
+	char current_char; 
 
-    while (lexer->position < lexer->length && is_whitespaces(lexer->input[lexer->position]))
-        lexer->position++;
-    if (lexer->position >= lexer->length)
-        return (NULL);
-    current_char = lexer->input[lexer->position];
-    
-    if (current_char == '\'' || current_char == '"')
-        return (handle_quotes(lexer, current_char))
-    if (current_char == '|' || current_char == '<' || current_char == '>' 
-        || current_char == '(' || current_char == ')')
-            return (handle_operator(lexer));
-    return (handle_word(lexer));
+	while (lexer->position < lexer->length && is_whitespaces(lexer->input[lexer->position]))
+		lexer->position++;
+	if (lexer->position >= lexer->length)
+		return (NULL);
+	current_char = lexer->input[lexer->position];
+	
+	if (current_char == '\'' || current_char == '"')
+		return (handle_quotes(lexer, current_char))
+	if (current_char == '|' || current_char == '<' || current_char == '>' 
+		|| current_char == '(' || current_char == ')')
+			return (handle_operator(lexer));
+	return (handle_word(lexer));
 }
 t_token     *handle_quotes(t_lexer *lexer, char quote_char)
 {
-    int     start;
-    int     content_length;
-    t_token *token;
-    char    content;
-    
-    lexer->position++;
-    start = lexer->position;
-    while (lexer->position < lexer->length && lexer->input[lexer->position] != quote_char)
-        lexer->position++;
-    if (lexer->position >= lexer->length)
-        ft_printf("Unclosed quote");
-    content_length = lexer->position - start;
-    content = ft_substr(lexer->input, start, content_length);
-    
-    lexer->position++;
+	int     start;
+	int     content_length;
+	t_token *token;
+	char    content;
+	
+	lexer->position++;
+	start = lexer->position;
+	while (lexer->position < lexer->length && lexer->input[lexer->position] != quote_char)
+		lexer->position++;
+	if (lexer->position >= lexer->length)
+		ft_printf("Unclosed quote");
+	content_length = lexer->position - start;
+	content = ft_substr(lexer->input, start, content_length);
+	
+	lexer->position++;
 
-    token = malloc(sizeof(t_token));
-    token->value = content;
-    token->quete_type = quote_char;
-    token->type = WORD_TOKEN;
+	token = malloc(sizeof(t_token));
+	token->value = content;
+	token->quete_type = quote_char;
+	token->type = WORD_TOKEN;
 
-    return (token);
+	return (token);
 }
 
 void handle_operator(t_parser *parser, t_token *op_token)
@@ -78,28 +78,71 @@ void handle_operator(t_parser *parser, t_token *op_token)
 
 t_token_type    classify_token(t_token *token)
 {
-    if (ft_strcmp(token->value, "|") == 0)
-        return (token->type = OP_TOKEN, token);
-    else if ((ft_strcmp(token->value, "<") == 0))
-        return  (token->type = OP_TOKEN, token->op_type = REDIR_IN);
-    else if ((ft_strcmp(token->value, ">") == 0))
-        return  (token->type = OP_TOKEN, token->op_type = REDIR_OUT);
-    else if ((ft_strcmp(token->value, ">>") == 0))
-        return  (token->type = OP_TOKEN, token->op_type = APPEND);
-    else if ((ft_strcmp(token->value, "<<") == 0))
-        return  (token->type = OP_TOKEN, token->op_type = HEREDOC);
-    else if ((ft_strcmp(token->value, "(") == 0))
-        return (L_PAREN_TOKEN);
-    else if ((ft_strcmp(token->value, ")") == 0))
-        return (R_PAREN_TOKEN);
-    else if (is_the first_token_in_command(token))
-        return (token->type = CMD_TOKEN, token)
-    else
-		return	(token->type = WORD_TOKEN, token);
+	if (ft_strcmp(token->value, "|") == 0)
+		return (OP_TOKE);
+	else if ((ft_strcmp(token->value, "<") == 0))
+		return  (REDIR_IN);
+	else if ((ft_strcmp(token->value, ">") == 0))
+		return  (REDIR_OUT);
+	else if ((ft_strcmp(token->value, ">>") == 0))
+		return  (APPEND);
+	else if ((ft_strcmp(token->value, "<<") == 0))
+		return  (HEREDOC);
+	else if ((ft_strcmp(token->value, "(") == 0))
+		return (L_PAREN_TOKEN);
+	else if ((ft_strcmp(token->value, ")") == 0))
+		return (R_PAREN_TOKEN);
+	else if (is_the first_token_in_command(token))
+		return (CMD_TOKEN)
+	else
+		return	(WORD_TOKEN);
+}
+
+t_parser *initialize_shunting_yard(void)
+{
+	t_parser *parser;
+	
+	parser = malloc(sizeof(t_parser));
+	parser->op_stack = creat_empty_stack();
+	parser->output_queue = creat_empty_queue();
+	return (parser);
+}
+
+void process_token(t_parser *parser, t_token *token)
+{
+	int op;
+
+	if (token->type == CMD_TOKEN || token->type == WORD_TOKEN)
+		enqueue(parser->output_queue, token);
+	else if (token->type == OP_TOKEN)
+		handle_operator(parser, token);
+	else if (token->type == L_PAREN_TOKEN)
+		push(parser->op_stack, token);
+	else if (token->type == L_PAREN_TOKEN)
+	{
+		while (!(is_empty(parser->op_stack)) && top((parser->op_stack)->type))
+		{
+			op = pop(parser->op_stack);
+			enqueue(parser->output_queue, op);
+		}
+		if (!is_empty(parser->op_stack))
+			pop(parser->op_stack);
+		else
+			report_error("Mismatched paentheses");
+	}
+}
+void handle_operator(t_parser *parser, t_token *op_token)
+{
+	while (!(is_empty(parser->op_stack)) && precedence(top(parser->op_stack)->type)
+		>= precedence(op_token) && top(parser->op_stack)->type != L_PAREN_TOKEN)
+	{
+		top_op = pop(parser->op_stack);	
+		
+	}
 }
 
 int main()
 {
-    char str = 's';
-    printf("%c", str);
+	char str = 's';
+	printf("%c", str);
 }
--- 42-cursus/circle_four/Minishell/yaamaich/minishell.h
+++ 42-cursus/circle_four/Minishell/yaamaich/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yaamaich <yaamaich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:00:24 by yaamaich          #+#    #+#             */
-/*   Updated: 2025/05/01 23:16:00 by yaamaich         ###   ########.fr       */
+/*   Updated: 2025/05/09 22:12:46 by yaamaich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 # include "libft/src/libft.h"
+
+
+#define top(stack) ((stack)->top->token)
+
 
 typedef struct s_lexer
 {
@@ -40,10 +44,37 @@ typedef enum {
 typedef struct s_token
 {
 	t_token_type	type;
-	t_token_type	op_type;
 	char			*value;
 	char			quete_type;
 	
 }				t_token;
+typedef	struct	s_stack_node
+{
+	t_token					*token;
+	struct s_stack_node		*next;
+}				t_stack_node;
 
-#endif
+typedef struct s_stack
+{
+	t_stack_node	*top;
+} 				t_stack;
+
+typedef struct s_queue_node
+{
+	t_token					*token;
+	struct s_queue_node		*next;
+}				t_queue_node;
+
+typedef struct s_queue
+{
+	t_queue_node	*front;
+	t_queue_node	*rear;
+}				t_queue;
+
+typedef struct s_parser
+{
+    t_stack  *op_stack;
+    t_queue  *output_queue;
+}				t_parser;
+
+#endif 
