d6308daa31c4b3669ae3f5ee20f9232662e2982e
yaamaich <amaicheyahya07@gmail.com>
2025-10-21 15:33:31 +0000

    push

--- philosfers/philo_routine.c
+++ philosfers/philo_routine.c
@@ -37,12 +37,7 @@ void	*philosopher_routine(void *arg)
 		philo_sleep_routine(p);
 	while (!is_simulation_over(p))
 	{
-		if (p->data->meals_required != -1
-			&& p->meals_count >= p->data->meals_required)
-			return (NULL);
 		log_action(p, "is thinking");
-		if (is_simulation_over(p))
-			return (NULL);
 		pthread_mutex_lock(p->fork_right);
 		log_action(p, "has taken a fork");
 		pthread_mutex_lock(p->fork_left);
