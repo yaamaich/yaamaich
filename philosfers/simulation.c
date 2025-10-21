f5d5bc94fb595a7f1bb591f29ab12c81ae72c6dc
yaamaich <amaicheyahya07@gmail.com>
2025-10-21 11:11:17 +0000

    push

--- philosfers/simulation.c
+++ philosfers/simulation.c
@@ -12,107 +12,123 @@
 
 #include "philo.h"
 
-void	ft_usleep(long long ms)
+void	ft_usleep(long long t)
 {
-	long long	begin_time;
+	long long	start;
+	long long	diff;
 
-	begin_time = current_time();
-	while (current_time() - begin_time < ms)
+	start = current_time();
+	diff = 0;
+	while (diff < t)
+	{
 		usleep(100);
+		diff = current_time() - start;
+	}
 }
 
 void	*philosopher_routine(void *arg)
 {
-	t_philo	*philosopher;
+	t_philo	*p;
+	int		is_even;
 
-	philosopher = (t_philo *)arg;
-	if ((philosopher->philo_id % 2) == 0)
-		philo_sleep_routine(philosopher);
+	p = (t_philo *)arg;
+	is_even = (p->philo_id % 2) == 0;
+	if (is_even)
+		philo_sleep_routine(p);
 	while (1)
 	{
-		log_action(philosopher, "is thinking");
-		pthread_mutex_lock(philosopher->fork_right);
-		log_action(philosopher, "has taken a fork");
-		pthread_mutex_lock(philosopher->fork_left);
-		log_action(philosopher, "has taken a fork");
-		log_action(philosopher, "is eating");
-		update_meal_status(philosopher);
-		ft_usleep(philosopher->data->eating_time);
-		pthread_mutex_unlock(philosopher->fork_right);
-		pthread_mutex_unlock(philosopher->fork_left);
-		check_philo_fullness(philosopher);
-		ft_usleep(philosopher->data->sleeping_time);
-		if (is_simulation_over(philosopher))
+		log_action(p, "is thinking");
+		pthread_mutex_lock(p->fork_right);
+		log_action(p, "has taken a fork");
+		pthread_mutex_lock(p->fork_left);
+		log_action(p, "has taken a fork");
+		log_action(p, "is eating");
+		update_meal_status(p);
+		ft_usleep(p->data->eating_time);
+		pthread_mutex_unlock(p->fork_right);
+		pthread_mutex_unlock(p->fork_left);
+		check_philo_fullness(p);
+		ft_usleep(p->data->sleeping_time);
+		if (is_simulation_over(p))
 			return (NULL);
 	}
 	return (NULL);
 }
 void	*single_philo_routine(void *arg)
 {
-	t_philo	*philosopher;
+	t_philo		*p;
+	long long	delay;
 
-	philosopher = (t_philo *)arg;
-	log_action(philosopher, "is thinking");
-	pthread_mutex_lock(philosopher->fork_right);
-	log_action(philosopher, "has taken a fork");
-	ft_usleep(philosopher->data->death_time + 1);
-	log_action(philosopher, "died");
-	pthread_mutex_unlock(philosopher->fork_right);
+	p = (t_philo *)arg;
+	log_action(p, "is thinking");
+	pthread_mutex_lock(p->fork_right);
+	log_action(p, "has taken a fork");
+	delay = p->data->death_time + 1;
+	ft_usleep(delay);
+	log_action(p, "died");
+	pthread_mutex_unlock(p->fork_right);
 	return (NULL);
 }
 void	*monitor_death(void *arg)
 {
-	t_table	*table;
-	int		checker;
+	t_table		*t;
+	int			i;
+	long long	time_diff;
+	int			meal_cnt;
 
-	table = (t_table *)arg;
+	t = (t_table *)arg;
 	while (1)
 	{
-		checker = 0;
+		i = 0;
 		usleep(500);
-		pthread_mutex_lock(&table->write_lock);
-		while (checker < table->philo_count)
+		pthread_mutex_lock(&t->write_lock);
+		while (i < t->philo_count)
 		{
-			if (table->full_philos == table->meals_required)
+			if (t->full_philos == t->meals_required)
 			{
-				table->simulation_stop = 1;
-				pthread_mutex_unlock(&table->write_lock);
+				t->simulation_stop = 1;
+				pthread_mutex_unlock(&t->write_lock);
 				return (NULL);
 			}
-			if (table->philosophers[checker].meals_count != table->meals_required
-				&& (current_time() - table->philosophers[checker].last_meal_time)
-				> table->death_time)
-				return (announce_death(checker, table), NULL);
-			checker++;
+			meal_cnt = t->philosophers[i].meals_count;
+			time_diff = current_time() - t->philosophers[i].last_meal_time;
+			if (meal_cnt != t->meals_required && time_diff > t->death_time)
+				return (announce_death(i, t), NULL);
+			i++;
 		}
-		pthread_mutex_unlock(&table->write_lock);
+		pthread_mutex_unlock(&t->write_lock);
 	}
 	return (NULL);
 }
 
-void	start_simulation(t_table *table)
+void	start_simulation(t_table *t)
 {
-	int			philo_iter;
-	pthread_t	death_monitor;
+	int			i;
+	pthread_t	monitor;
+	int			is_single;
 
-	philo_iter = 0;
-	table->start_time = current_time();
-	if (table->philo_count == 1)
+	i = 0;
+	t->start_time = current_time();
+	is_single = (t->philo_count == 1);
+	if (is_single)
 	{
-		pthread_create(&table->philosophers[0].philo_thread, NULL,
-			single_philo_routine, (void *)&table->philosophers[0]);
-		pthread_join(table->philosophers[0].philo_thread, NULL);
+		pthread_create(&t->philosophers[0].philo_thread, NULL,
+			single_philo_routine, (void *)&t->philosophers[0]);
+		pthread_join(t->philosophers[0].philo_thread, NULL);
 		return ;
 	}
-	while (philo_iter < table->philo_count)
+	while (i < t->philo_count)
 	{
-		pthread_create(&table->philosophers[philo_iter].philo_thread, NULL,
-			philosopher_routine, (void *)&table->philosophers[philo_iter]);
-		philo_iter++;
+		pthread_create(&t->philosophers[i].philo_thread, NULL,
+			philosopher_routine, (void *)&t->philosophers[i]);
+		i++;
 	}
-	philo_iter = 0;
-	pthread_create(&death_monitor, NULL, monitor_death, (void *)table);
-	while (philo_iter < table->philo_count)
-		pthread_join(table->philosophers[philo_iter++].philo_thread, NULL);
-	pthread_join(death_monitor, NULL);
+	i = 0;
+	pthread_create(&monitor, NULL, monitor_death, (void *)t);
+	while (i < t->philo_count)
+	{
+		pthread_join(t->philosophers[i].philo_thread, NULL);
+		i++;
+	}
+	pthread_join(monitor, NULL);
 }
